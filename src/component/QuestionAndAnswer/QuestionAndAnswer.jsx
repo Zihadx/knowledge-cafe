d7367f6a0ab32868d3dc5394cca3d27dd485c853
zihadx <blankzihad@gmail.com>
2023-04-01 19:36:20 +0600

    perpose of useeffect

--- src/component/QuestionAndAnswer/QuestionAndAnswer.jsx
+++ src/component/QuestionAndAnswer/QuestionAndAnswer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const QuestionAndAnswer = () => {
   return (
     <div>
-        <h1 className="text-5xl font-bold text-sky-600 text-center mt-12">QNA section</h1>
+        <h1 className="text-5xl font-bold text-sky-700 text-center mt-12">QNA section</h1>
       <div>
         <h1 className="text-3xl font-bold mt-8">
           What is the difference between state and props in React?
@@ -26,12 +26,13 @@ const QuestionAndAnswer = () => {
       </div>
       <div className="mt-8">
         <h1 className="text-3xl font-bold mt-8">
-         how does useState work in react?
+        Purpose of useEffect other than fetching data.
         </h1>
         <p className="font-semibold text-xl mt-4">
-         <span className="font-bold ms-8">Ans: </span> useState is a Hook that allows functional components to manage state.This is used to add state to a functional component, and the state can be updated using the set function that is returned by the Hook.
+         <span className="font-bold ms-8">Ans: </span> useEffect is a Hook in React that allows you to perform side effects in function components. While useEffect is commonly used for fetching data, it can also be used for updating the title of the page, setting up event listeners, controlling component animations and others. Main purpose of useEffect- Allows to perform side effects in function components, and it can be used for many purposes beyond fetching data.
         </p>
       </div>
+      
 
 
     </div>
