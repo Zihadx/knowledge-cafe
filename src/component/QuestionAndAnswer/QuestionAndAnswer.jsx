0b9c2e914327899a81cbd4a0e547df6af0851bd5
zihadx <blankzihad@gmail.com>
2023-04-01 19:37:40 +0600

    complet question and answer

--- src/component/QuestionAndAnswer/QuestionAndAnswer.jsx
+++ src/component/QuestionAndAnswer/QuestionAndAnswer.jsx
@@ -32,7 +32,14 @@ const QuestionAndAnswer = () => {
          <span className="font-bold ms-8">Ans: </span> useEffect is a Hook in React that allows you to perform side effects in function components. While useEffect is commonly used for fetching data, it can also be used for updating the title of the page, setting up event listeners, controlling component animations and others. Main purpose of useEffect- Allows to perform side effects in function components, and it can be used for many purposes beyond fetching data.
         </p>
       </div>
-      
+      <div className="mt-8">
+        <h1 className="text-3xl font-bold mt-8">
+        How Does React work?
+        </h1>
+        <p className="font-semibold text-xl mt-4">
+         <span className="font-bold ms-8">Ans: </span> React is a popular JavaScript library that is used to build user interfaces. React uses a virtual DOM to manage the state and rendering of components, which makes it efficient and fast. React works according to the following step- component rendering, virtual DOM, state management, event handling, performance optimizations.
+        </p>
+      </div>
 
 
     </div>
